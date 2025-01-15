da798f2dc350e3869087622e75cde6d5b2efcffa
R1bo - Ewen <ewen.jezequel1@gmail.com>
2025-01-15 20:02:55 +0100

    Clean CityMood and Scrapper, creation Orchestrator

--- src/Scrapper.py
+++ src/Scrapper.py
@@ -1,26 +1,35 @@
+from urllib.request import UnknownHandler
 from DBManager import Manager, DBArticle, DBTables
-from datetime import datetime
+from datetime import datetime, date
+import feedparser
 
-def check_article_is_in_base_by_url(url:str, db: Manager) -> bool:
-    if db.get_article_by_url(url) != []:
-        return True
-    else: 
-        return False
-    
-def get_news(city:str, db: Manager, news_feed):
-        cityPurged = city.lower()
-        for entry in news_feed.entries:
-            if check_article_is_in_base_by_url(entry.link, db):
-                if ((city in entry.description) or (city in entry.title) or (city in entry.link) 
-                    or (cityPurged in entry.description) or (cityPurged in entry.title) or (cityPurged in entry.link)):
-                    print(f"Article {entry.title} n'est pas déjà dans la table")
-                    db.insert_data(
-                        DBTables.ARTICLE,
-                        DBArticle(
-                            publication_date=datetime.date.now,
-                            url=entry.link,
-                            title=entry.title,
-                            description=entry.description,
-                            content=""
+class Scrapper():
+    def __init__(self, news_feed_url: str, target_city: str, db: Manager) -> None:
+        self._news_feed = feedparser.parse(news_feed_url)
+        self._city : str = target_city
+        self._db : Manager = db
+        pass
+        
+    def check_article_is_in_base_by_url(self, url:str, db: Manager) -> bool:
+        if db.get_article_by_url(url) != []:
+            return True
+        else: 
+            return False
+        
+    def get_news(self) -> None:
+            cityPurged = self._city.lower()
+            for entry in self._news_feed.entries:
+                if self.check_article_is_in_base_by_url(entry.link, self._db) == False:
+                    if ((self._city in entry.description) or (self._city in entry.title) or (self._city in entry.link) 
+                        or (cityPurged in entry.description) or (cityPurged in entry.title) or (cityPurged in entry.link)):
+                        print(f"Article {entry.title} n'est pas déjà dans la table")
+                        self._db.insert_data(
+                            DBTables.ARTICLE,
+                            DBArticle(
+                                publication_date=datetime.now(),
+                                url=entry.link,
+                                title=entry.title,
+                                description=entry.description,
+                                content=""
+                            )
                         )
-                    )
